efd2970b6945a039619b0d43bea20d68d3681724
Blxckmage <fazadoang15@gmail.com>
2023-03-10 22:10:01 +0700

    feat: update routes

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,22 @@ use Illuminate\Support\Facades\Route;
 
 // Login Pages
 use App\Http\Controllers\BookController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
 
 
 // Admin Pages
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ApprovalController;
-use App\Http\Controllers\RegisterController;
 
 // Main Pages
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\AdminBookController;
-use App\Http\Controllers\PeminjamanController;
 
 // User
+use App\Http\Controllers\PeminjamanController;
 use App\Http\Controllers\AdminCategoryController;
+use App\Http\Controllers\AdminEmployeeController;
 use App\Http\Controllers\AdminPublisherController;
 
 /*
@@ -31,13 +33,14 @@ use App\Http\Controllers\AdminPublisherController;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->middleware('auth');
 
-Route::get('/home', function () {
-    return view('home');
-});
+// General
+
+// Route::get('/', function () {
+//     return view('home');
+// })->middleware('auth');
+
+Route::get('/', [HomeController::class, 'index'])->middleware('auth');
 
 Route::get('/account/{name}', [UserController::class, 'index']);
 
@@ -46,13 +49,15 @@ Route::get('/books/{id}', [BookController::class, 'show']);
 
 Route::get('/borrowing', [PeminjamanController::class, 'index']);
 Route::post('/borrowing/search', [PeminjamanController::class, 'search']);
+Route::get('/borrowing/borrow/{id}', [PeminjamanController::class, 'borrow']);
+Route::post('/borrowing/borrow', [PeminjamanController::class, 'store']);
+
+
+// Employee
 
 Route::get('/approval', [ApprovalController::class, 'index'])->name('approval.index');
 Route::post('/approval/approve/{id}', [ApprovalController::class, 'approve']);
 Route::post('/approval/reject/{id}', [ApprovalController::class, 'reject']);
-
-Route::get('/borrowing/borrow/{id}', [PeminjamanController::class, 'borrow']);
-Route::post('/borrowing/borrow', [PeminjamanController::class, 'store']);
 
 Route::get('/tambah-kategori', [AdminCategoryController::class, 'index']);
 Route::post('/tambah-kategori', [AdminCategoryController::class, 'store']);
@@ -65,6 +70,16 @@ Route::delete('/tambah-penerbit/{publisher}', [AdminPublisherController::class, 
 Route::get('/tambah-buku', [AdminBookController::class, 'index']);
 Route::post('/tambah-buku', [AdminBookController::class, 'store']);
 Route::delete('/tambah-buku/{book}', [AdminBookController::class, 'destroy']);
+
+
+// Admin
+
+Route::get('/tambah-petugas', [AdminEmployeeController::class, 'index']);
+Route::post('/tambah-petugas/update/{id}', [AdminEmployeeController::class, 'update']);
+Route::post('/tambah-petugas', [AdminEmployeeController::class, 'search']);
+
+
+// Login
 
 Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
