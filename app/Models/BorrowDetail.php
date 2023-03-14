080c3364b11c4d8104902ba084e3d44e0b06b6b2
Blxckmage <fazadoang15@gmail.com>
2023-03-14 08:15:10 +0700

    feat: add user function for borrow_details

--- app/Models/BorrowDetail.php
+++ app/Models/BorrowDetail.php
@@ -26,4 +26,9 @@ class BorrowDetail extends Model
     {
         return $this->belongsTo(Book::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
