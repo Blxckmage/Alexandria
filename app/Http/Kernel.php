5b5800f24e2773a9cb282165c90d4b2665be7f37
Blxckmage <fazadoang15@gmail.com>
2023-03-14 08:12:32 +0700

    feat: add middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,9 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'employee' => \App\Http\Middleware\EmployeeMiddleware::class,
+        'borrower' => \App\Http\Middleware\BorrowerMiddleware::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
